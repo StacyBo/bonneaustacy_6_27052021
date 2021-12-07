972e132f600cf63b1d138c68ab88edcf2fde9a95
Stacy Bonneau <bonneau.stacy@gmail.com>
2021-12-07 17:11:17 +0100

    ajout fichier backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
 
+const path = require("path");
+
+//const Sauce = require('./models/sauces');
+
+// create and configure express app
 const app = express();
-const stuffRoutes = require('./routes/stuff')
 
+// CORS middleware
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -11,6 +19,10 @@ app.use((req, res, next) => {
   next();
 });
 
+// Parse the body of the request
+app.use(bodyParser.json());
+
+// Connection a la base Data 
 mongoose.connect('mongodb+srv://StacyB:HelloYou@cluster0.gxlnh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
@@ -19,10 +31,11 @@ mongoose.connect('mongodb+srv://StacyB:HelloYou@cluster0.gxlnh.mongodb.net/myFir
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const userRoutes = require('./routes/user');
-
-app.use('/api/stuff', stuffRoutes);
+// Add routes
+app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 
 module.exports = app;
